44004771ad6a3c250b8231d0457e5be57e97bb2f
Tanmay Patel <tyson@Tanmays-MacBook-Pro.local>
2015-04-14 17:03:21 -0400

    Initial Commit

---
+++ helloworldapp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworldapp
+//
+//  Created by Tanmay Patel on 4/14/15.
+//  Copyright (c) 2015 Tanmay Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ helloworldapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  helloworldapp
+//
+//  Created by Tanmay Patel on 4/14/15.
+//  Copyright (c) 2015 Tanmay Patel. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
